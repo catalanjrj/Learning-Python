de437a78e6592a7c5fdebd3cfa6bcbb011e9a0ce
jcats <catalanjrj@protonmail.com>
2019-04-16 16:49:14 -0400

    reduce the number of target.writes to one

--- ex16.py
+++ ex16.py
@@ -21,12 +21,18 @@ line3 = input("line 3: ")
 
 print("I'm going to write these to the file.")
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write(f""" 
+{line1}\n
+{line2}\n
+{line3}\n
+""")
+
+#target.write(line1)
+#target.write("\n")
+#target.write(line2)
+#target.write("\n")
+#target.write(line3)
+#target.write("\n")
 
 print("And finally, we close it.")
 target.close()
