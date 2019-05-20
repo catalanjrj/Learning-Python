452251968dfa96df16202a2237f0b3643527a48a
jcats <catalanjrj@protonmail.com>
2019-05-20 16:58:03 -0400

    Moving through ex20

--- ex21.py
+++ ex21.py
@@ -1,2 +1,17 @@
-def add(a,b):
+def add(a, b):
     print(f"ADDING{a} + {b}")
+    return a + b 
+
+def subtract(a, b): 
+    print(f"SUBTRACTING {a} -{b}")
+    return a - b
+
+def multiply(a, b):
+    print(f"MULTIPLYING {a} * {b}")
+    return a * b
+
+def divide(a, b): 
+    print(f"DIVIDING {a} /{b}")
+    return a / b
+
+print("Let's do some math with just functions!")
