83129f80a3379d70e4b83418b92b4b6e88dfa5bc
jcats <catalanjrj@protonmail.com>
2019-06-24 15:08:50 -0400

    study drills

--- ex21.py
+++ ex21.py
@@ -15,3 +15,17 @@ def divide(a, b):
     return a / b
 
 print("Let's do some math with just functions!")
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
+
+# A puzzle for the extra credit, type it anyway.k 
+print("Here is a puzzle.") 
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+print("That becomes: ", what, "Can you do it by hand?")
